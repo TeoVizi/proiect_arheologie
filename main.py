f7e1c31baee0ab4d62788333c7ab0f7baf9c0e84
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2025-01-13 01:22:54 +0200

    distilBERT cu raspuns la intrebare

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+import torch
 
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+model_name = "distilbert-base-uncased-distilled-squad"
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-text = "This is a sample sentence."
-inputs = tokenizer(text, return_tensors="pt")
+context = """
+Everyday I wake up at 10am in order to go to school and my name is Alex.
+"""
+
+question = "What is my name?"
+
+inputs = tokenizer(question, context, return_tensors="pt")
 
 outputs = model(**inputs)
+
+start_idx = torch.argmax(outputs.start_logits)
+end_idx = torch.argmax(outputs.end_logits) + 1
+
+answer = tokenizer.decode(inputs["input_ids"][0][start_idx:end_idx])
+print(question)
+print(answer)
